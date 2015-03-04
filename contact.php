2da91587b949a2bf6811e045188c2f095f4561bc
BuildTools <unconfigured@null.spigotmc.org>
2015-03-04 10:14:47 +0100

    Add Files
    All files

---
+++ contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Form@Cup</title>
+		<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<header>
+			<img src="images/ecran_logo.png" alt="logo Forma=@Cup" id="logo" />
+			<h1>Form@Cup</h1>
+			<div id="share">
+
+			</div>
+		</header>
+		<br>
+
+		<nav>
+			<ul class="menu">
+				<li id="accueil"><a href="index.php">Accueil</a></li>
+				<li id="formations"><a href="formation.php">Formations</a></li>
+				<li id="compagnie"><a href="compagnie.php">Compagnie</a></li>
+				<li id="contact"><a href="contact.php">Contact</a></li>
+			</ul>
+		</nav>
+		<br>
+
+		<section>
+			<br>
+			<h1>Fiche de contact</h1>
+			<br>
+			<br>
+			<div id="presentation">
+				<p>Voicis nos coordonnées pour nous contacter.</p>
+				<br>
+				<p>blblbbll@boblo.fr ou Tel : 05 06 04 08 06</p>
+			</div>
+			<br>
+		</section>
+	</body>
+
+	<footer>
+	</footer>
+</html>
---
+++ formation.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Form@Cup</title>
+		<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<header>
+			<img src="images/ecran_logo.png" alt="logo Forma=@Cup" id="logo" />
+			<h1>Form@Cup</h1>
+			<div id="share">
+
+			</div>
+		</header>
+		<br>
+
+		<nav>
+			<ul class="menu">
+				<li id="accueil"><a href="index.php">Accueil</a></li>
+				<li id="formations"><a href="#">Formations</a></li>
+				<li id="compagnie"><a href="#">Compagnie</a></li>
+				<li id="contact"><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+		<br>
+
+		<section>
+			<br>
+			<h1>Fiche de contact</h1>
+			<br>
+			<br>
+			<div id="typeforma">
+			<?php
+				foreach ($tabPersonnes as $indice => $personne) {
+					print('<tr><td>'.$personne['nom'].'</td>');
+					print('<td>'.$personne['prenom'].'</td>');
+					print('<td>'.$personne['mail'].'</td>');
+					print('<td>'.$personne['ddn'].'</td>');
+					print('<td><a href="liste.php?act=DEL" title="Supprimer"><img class="litleimg" src="supr.png"/></a></td>');
+					print('<td><a href="fiche.php?act=MOD&idpersonne='.$indice.'" title="Modifier"><img class="litleimg" src="mod.png"/></a></td>');
+					print('<td><a href="fiche.php?act=VIEW&idpersonne='.$indice.'" title="Visualiser"><img class="litleimg" src="view.png"/></a></td></tr>');
+				}
+			?>
+			</div>
+			<br>
+		</section>
+	</body>
+
+	<footer>
+	</footer>
+</html>
